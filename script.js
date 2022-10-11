9947adc06d2db075697ab23982b94b4dc7471bb1
Lukas Volgger <55922592+LukasVolgger@users.noreply.github.com>
2022-10-11 20:56:36 +0200

    translate to englisch and add acessability tags

--- script.js
+++ script.js
@@ -50,20 +50,20 @@ function generateStaticHTML() {
     return `
         <header class="border-spacing">
             <div class="header-title">
-                <span class="header-title-text">Fotogallerie</span>
+                <span class="header-title-text">Photo Gallery</span>
                 <span class="header-title-subtext">by Lukas Volgger</span>
             </div>
             <div class="header-subcontainer">
                 <div id="search-bar-container">
                     <a href="#" class="search-bar-link"><img src="./img/icons/search.svg" alt="search" id="search-icon" class="icons"></a>
-                    <input type="text" placeholder="Suche" class="element-height" id="search-bar">
+                    <input type="text" placeholder="Search" class="element-height" id="search-bar">
                 </div>
             </div>
         </header>
         <main id="main" class="border-spacing">
             <div id="img-container" class="img-container">
                 <a href="#" id="icon-container-up" class="icon-container-up">
-                    <img src="./img/icons/double_up_arrow.svg">
+                    <img src="./img/icons/double_up_arrow.svg" alt="Arrow Up" title="Up">
                 </a>
                 <div id="focus-img-container" class="d-none focus-img-container"></div>
             </div>
@@ -78,7 +78,7 @@ function generateImages() {
         let imgContainer = document.getElementById('img-container');
 
         imgContainer.innerHTML += `
-            <img src="${images[i]}" id="img-${i}" class="gallery-img" onclick="openImage(${i})">
+            <img src="${images[i]}" id="img-${i}" class="gallery-img" onclick="openImage(${i})" alt="${images[i]}">
         `;
     }
 }
@@ -127,13 +127,13 @@ function generateFocusImage(image) {
     return `
         <img src="${images[image]}" alt="Focus-Image" class="focus-img" id="focus-img-${image}" onclick="event.stopPropagation()">
         <div class="icon-container close-icon" onclick="closeImageFocus()">
-            <img src="./img/icons/close.svg">
+            <img src="./img/icons/close.svg" alt="Close icon" title="Close">
         </div>
         <div class="icon-container left-arrow-icon" onclick="nextImageLeft()">
-            <img src="./img/icons/left_arrow.svg">
+            <img src="./img/icons/left_arrow.svg" alt="Arrow Left" title="Left">
         </div>
         <div class="icon-container right-arrow-icon" onclick="nextImageRight()">
-            <img src="./img/icons/right_arrow.svg">
+            <img src="./img/icons/right_arrow.svg" alt="Arrow Right" title="Right">
         </div>
     `;
 }
